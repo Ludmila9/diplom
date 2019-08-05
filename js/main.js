c1adb9a7a2d0ca9d3fc892f8abf3cd7c9d9b80af
Ludmila9 <dzubaludmila9@gmail.com>
2019-08-06 00:05:46 +0300

    change main.js - onmouseover and onmouseout around circle

--- js/main.js
+++ js/main.js
@@ -78,35 +78,113 @@
 	//   }
 	// }
 
-	//new slider
-	// $('.ba-slider-new').masonry({
-	// 	itemSelector: '.portfolio-item',
-	// });
-	//  function initSlider() {
-	//  	$('.ba-slider-new').slick({
-	// 	  slidesToShow: 1,
-	// 	  slidesToScroll: 1,
-	// 	  arrows: false,
-	// 	  fade: true,
-	// 	  asNavFor: '.ba-slider-new-nav'
-	// 	});
-		
-	//  };
-	//  $('.ba-slider-new-nav').slick({
-	// 	  slidesToShow: 6,
-	// 	  slidesToScroll: 1,
-	// 	  asNavFor: '.ba-slider-new',
-	// 	  dots: false,
-	// 	  // centerMode: true,
-	// 	  focusOnSelect: true
-	// 	});
-		
-	//  $('.items-navi button').on('click', function(){
-	//  	initSlider();
-	//  	$('.items-navi button').addClass('nav-btn');
-	//  })
-
+	
 	
 
 })(jQuery); 
 
+
+
+
+// circle on / out
+
+// // В консоль все выводится, но не срабатывает
+// let item = document.querySelectorAll('.head-soc__item');	
+// let circle = document.getElementById('circle');
+// // let circle = item.children[0].children
+// let circleParent = circle.parentNode;
+// console.log(circleParent);
+// let circleLength = circle.getTotalLength();
+// console.log(circleLength);
+// for (var i = 0; i < item.length; i ++); {
+
+// 	if (item.type == 'mouseover') {
+// 		item[i].onmouseover = function(){
+// 			circle.classList.add('fill');
+// 		};
+// 	};
+// 	if (item.type == 'mouseover') {
+// 		item[i].onmouseout = function(){
+// 			circle.classList.remove('fill');
+// 		};
+// 	};
+// }; 
+
+// item.onmouseover = function(){	
+// 	item.children[0].children.classList.add('fill');
+// };
+// item.onmouseout = function(){
+// 	item.children[0].children.classList.remove('fill');
+// };
+// console.log(item);
+// console.log(circle);
+// // console.log(circleLength);
+// console.log(item.children[0].children);
+// console.log(circleParent);
+// console.log(circleParent.parentNode);
+
+
+
+
+// // Пыталась достучаться до ребенка каждой li-шки,
+// // консоль пишет ошибку: Uncaught TypeError: item.getElementsByClassName is not a function
+
+// let item = document.getElementsByClassName('.head-soc__item');
+// let childItem = item.getElementsByClassName('.icon-circle');
+// let circle = childItem.getElementById('#circle');
+// console.log(item);
+// console.log(childItem);
+// console.log(circle);
+
+// // for (var i = 0; i < item.length; i ++); {
+// 	item.onmouseover = function(){	
+// 		circle.classList.add('fill');
+// 	};
+// 	item.onmouseout = function(){
+// 		circle.classList.remove('fill');
+// 	};
+// // };
+
+
+
+
+// // Если прописать так, то:
+// // 1) срабатывает только onmouseover, onmouseout срабатывает
+// // только на первом элементе
+// // 2) срабатывает сразу на всех элементах
+// let circle = document.querySelector('.head-soc__item > .icon-circle > #circle');
+// //или так: let circle = document.getElementById('circle');
+// console.log(circle);
+// let circleLength = circle.getTotalLength();
+// console.log(circleLength);
+// circle.onmouseover = function(){	
+// 	this.classList.add('fill');
+// };
+// circle.onmouseout = function(){
+// 	this.classList.remove('fill');
+// };
+
+
+
+
+// Пыталась создать массив, присвоить item каждый элемент из цикла по этому массиву, 
+// при console.log(item);	выводит undefined
+// let items = document.querySelector('.head-soc__item');
+
+// // console.log(items);
+// // console.log(childItem);
+// // console.log(circle);
+// // console.log(circleLength);
+// for (var i = 0; i < items.length; i ++); {
+// 	let item = items[i];
+// 	// console.log(item);		//выводит undefined
+// 	let childItem = item.querySelector('.icon-circle');
+// 	let circle = childItem.querySelector('#circle');
+// 	let circleLength = circle.getTotalLength();
+// 	item.onmouseover = function(){	
+// 		circle.classList.add('fill');
+// 	};
+// 	item.onmouseout = function(){
+// 		circle.classList.remove('fill');
+// 	};
+// };
